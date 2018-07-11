2b228bfdddbeb21c212188b7cf8b6d148178c16e
Jamie Cansdale <jcansdale@github.com>
2018-07-12 00:22:09 +0100

    Add a `File > New > Temporary Project...` command
    By default the project will be created in:
    `{ProjectsLocation}\Temp\yyyy-MM-dd\`, where `{ProjectsLocation}` is
    the default location for your projects. This will hopefully make
    projects discoverable by date and hopefully avoid ending up with
    `ClassLibrary17` (depending on how many temporary projects you create
    in a day).

---
+++ TemporaryProjects/NewTempProjectCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.ComponentModel.Design;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace TemporaryProjects
+{
+    internal sealed class NewTempProjectCommand
+    {
+        readonly DTE dte;
+
+        public const int CommandId = 0x0100;
+        public static readonly Guid CommandSet = new Guid("f5e2d15e-a980-4a05-81ec-fda9c6f91a9c");
+
+        private NewTempProjectCommand(IMenuCommandService commandService, DTE dte)
+        {
+            this.dte = dte;
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand((s, e) => Execute(dte), menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            await package.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            var commandService = (IMenuCommandService)await package.GetServiceAsync((typeof(IMenuCommandService)));
+            var dte = (DTE)await package.GetServiceAsync((typeof(DTE)));
+            Instance = new NewTempProjectCommand(commandService, dte);
+        }
+
+        // You can test this command in the current VS instance like this:
+        // https://github.com/jcansdale/TestDriven.Net-Issues/wiki/Test-With...VS-SDK
+        [STAThread]
+        static void Execute(DTE dte)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var projectsLocation = dte.Properties["Environment", "ProjectsAndSolution"].Item("ProjectsLocation");
+            var location = (string)projectsLocation.Value;
+            var tempPath = string.Format(@"Temp\{0:yyyy-MM-dd}\", DateTime.Now);
+            var tempLocation = Path.Combine(location, tempPath);
+
+            try
+            {
+                projectsLocation.Value = tempLocation;
+                dte.ExecuteCommand("File.NewProject");
+            }
+            finally
+            {
+                projectsLocation.Value = location;
+            }
+        }
+
+        public static NewTempProjectCommand Instance
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ TemporaryProjects/NewTempProjectCommandPackage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace TemporaryProjects
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class NewTempProjectCommandPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "5abd62ee-e60e-45a5-bd01-9c873436490f";
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await NewTempProjectCommand.InitializeAsync(this);
+        }
+    }
+}
