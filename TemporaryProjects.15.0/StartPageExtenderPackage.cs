539a6eb5db631a76d9aa524b10240ddfb28d50bd
Jamie Cansdale <jcansdale@github.com>
2018-07-24 11:04:55 +0100

    Only auto-load when the StartPage is visible
    It appears we can use the StartPage PersistenceSlot as a UIContext.

--- TemporaryProjects.15.0/StartPageExtenderPackage.cs
+++ TemporaryProjects.15.0/StartPageExtenderPackage.cs
@@ -2,7 +2,6 @@
 using System.Threading;
 using System.Runtime.InteropServices;
 using System.Diagnostics.CodeAnalysis;
-using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
 using Task = System.Threading.Tasks.Task;
 
@@ -12,10 +11,11 @@ namespace TemporaryProjects
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [Guid(PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.ShellInitialized_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(StartPageToolWindowString, PackageAutoLoadFlags.BackgroundLoad)]
     public sealed class StartPageExtenderPackage : AsyncPackage
     {
         public const string PackageGuidString = "3dd793ab-452e-4e50-9eaf-57ad755aff1d";
+        public const string StartPageToolWindowString = "387cb18d-6153-4156-9257-9ac3f9207bbe";
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
