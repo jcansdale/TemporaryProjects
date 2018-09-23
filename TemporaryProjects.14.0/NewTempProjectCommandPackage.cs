abde624a031729043b65b4c3a51e957b01bbd981
Jamie Cansdale <jcansdale@github.com>
2018-09-23 20:47:34 +0100

    Split into VSIX packaging and extension projects (#6)
    Split into separate VSIX packaging an extension projects
    
    - Use `Madskristensen.VisualStudio.SDK` NuGet package
    - Move constants to `PackageGuids` and `PackageIds`
    - Split into separate VSIX packaging an extension projects
    - Set `CreateVsixContainer` and `DeployExtension` for release configuration

--- TemporaryProjects.14.0/NewTempProjectCommandPackage.cs
+++ TemporaryProjects.14.0/NewTempProjectCommandPackage.cs
@@ -10,12 +10,10 @@ namespace TemporaryProjects
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [Guid(PackageGuidString)]
+    [Guid(PackageGuids.newTempProjectCommandPackageString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
     public sealed class NewTempProjectCommandPackage : AsyncPackage
     {
-        public const string PackageGuidString = "5abd62ee-e60e-45a5-bd01-9c873436490f";
-
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             await NewTempProjectCommand.InitializeAsync(this);
