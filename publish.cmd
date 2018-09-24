5ecce4a98abefddcaae3841851c57a688d1f7b9c
Jamie Cansdale <jcansdale@github.com>
2018-09-25 00:36:04 +0100

    Hard code VSInstallDir to VS 2017 Enterprise

--- publish.cmd
+++ publish.cmd
@@ -1,3 +1,4 @@
+@set VSInstallDir=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise
 @set path="%VSInstallDir%\VSSDK\VisualStudioIntegration\Tools\Bin";%path%
 VsixPublisher login -publisherName JamieCansdale -personalAccessToken %personalAccessToken%
 VsixPublisher publish -payload TemporaryProjects.Vsix\bin\Debug\TemporaryProjects.vsix -publishManifest publishManifest.json
