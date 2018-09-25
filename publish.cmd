cc2253ff747be849a3126ff293cb8e3f4a2131e5
Jamie Cansdale <jcansdale@github.com>
2018-09-25 20:49:28 +0100

    Publish the Release configuration

--- publish.cmd
+++ publish.cmd
@@ -2,4 +2,4 @@
 @set VSInstallDir=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise
 @set path="%VSInstallDir%\VSSDK\VisualStudioIntegration\Tools\Bin";%path%
 VsixPublisher login -publisherName JamieCansdale -personalAccessToken %personalAccessToken%
-VsixPublisher publish -payload TemporaryProjects.Vsix\bin\Debug\TemporaryProjects.vsix -publishManifest publishManifest.json
+VsixPublisher publish -payload TemporaryProjects.Vsix\bin\Release\TemporaryProjects.vsix -publishManifest publishManifest.json
