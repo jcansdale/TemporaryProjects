8257e65fb3b3c4816843a94610c12d04ee3d1d28
Jamie Cansdale <jcansdale@github.com>
2018-10-04 22:14:26 +0100

    Convert publish.cmd to Publish.ps1
    Locate VSSDK using VSSetup module.

---
+++ Publish.ps1
@@ -0,0 +1,20 @@
+[CmdletBinding(SupportsShouldProcess=$true)]
+param (
+	[parameter(Mandatory=$true,
+		HelpMessage="Create token using https://dev.azure.com/github-editor-tools/_usersSettings/tokens?action=edit with Organization: All accessible organizations, Selected scopes: Marketplace (publish)")]
+	[string] $personalAccessToken
+	,
+	[parameter(
+		HelpMessage="Publisher name which must match the one in the manifest")]
+	[string] $publisherName="JamieCansdale"
+	,
+	[parameter(
+		HelpMessage="Path to the .vsix file for publishing")]
+	[string] $payload="$PSScriptRoot\TemporaryProjects.Vsix\bin\Release\TemporaryProjects.vsix"
+	,
+	[parameter(
+		HelpMessage="Path to the publish manifest .json file")]
+	[string] $publishManifest = "$PSScriptRoot\publishManifest.json"
+)
+
+& $PSScriptRoot\Publish-Vsix.ps1 -WhatIf:$WhatIf -Confirm:$Confirm -publisherName:$publisherName -personalAccessToken:$personalAccessToken -payload:$payload -publishManifest:$publishManifest
--- publish.cmd
+++
@@ -1,5 +0,0 @@
-@set personalAccessToken=%~1
-@set VSInstallDir=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise
-@set path="%VSInstallDir%\VSSDK\VisualStudioIntegration\Tools\Bin";%path%
-VsixPublisher login -publisherName JamieCansdale -personalAccessToken %personalAccessToken%
-VsixPublisher publish -payload TemporaryProjects.Vsix\bin\Release\TemporaryProjects.vsix -publishManifest publishManifest.json
